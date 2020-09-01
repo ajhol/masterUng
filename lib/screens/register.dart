923d9ed3063450c11a05a7e9088e85a8c1a26211
ajhol <abdloh@gmail.com>
2020-09-01 15:24:16 +0700

    ok

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -6,12 +6,25 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+// Explict
+
+// Method
+  Widget registerButton() {
+    return IconButton(
+      icon: Icon(Icons.cloud_upload),
+      onPressed: () {
+        print('You Click Upload');
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.yellow.shade600,
         title: Text('Register'),
+        actions: [registerButton()],
       ),
       body: Text('body'),
     );
